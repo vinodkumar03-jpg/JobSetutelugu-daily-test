e3224d9e94f3d5516d6bc2d0b12b12e4b5d3fb99
vinodkumar03-jpg <vinodkumarvankudoth011@gmail.com>
2026-07-21 12:37:43 +0530

    Add startTest function for user greeting

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// JobSetu Telugu JavaScript
+ function startTest() {
+
+    let name = document.querySelector("input[type='text']").value;
+
+    if(name==""){
+        alert("Please enter your Name");
+        return;
+    }
+
+    alert("Welcome " + name + "!\n\nYour test will start soon.");
+
+}
