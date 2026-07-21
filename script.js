82347bb1960c71978eab4e606951c49cfb4d746b
vinodkumar03-jpg <vinodkumarvankudoth011@gmail.com>
2026-07-21 12:57:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ function startTest() {
     let name = document.getElementById("studentName").value;
     let mobile = document.getElementById("mobileNumber").value;
 
-    if (name.trim() === "") {
+    if(name.trim()==""){
         alert("Please enter your Name");
         return;
     }
 
-    if (mobile.length !== 10 || isNaN(mobile)) {
+    if(mobile.length!=10 || isNaN(mobile)){
         alert("Please enter a valid 10-digit Mobile Number");
         return;
     }
 
-    alert("Welcome " + name + "!\n\nYour test will start soon.");
+    localStorage.setItem("studentName",name);
+    localStorage.setItem("mobile",mobile);
+
+    window.location.href="test.html";
 
 }
