2bd26076a983adcacf2e0c66680f9aa607ac609a
vinodkumar03-jpg <vinodkumarvankudoth011@gmail.com>
2026-07-21 13:08:22 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,34 +1,75 @@
+let timeLeft = 25 * 60;
 let currentQuestion = 0;
 let questions = [];
 let answers = [];
 
+// Welcome message
 document.getElementById("student").innerHTML =
 "Welcome, " + localStorage.getItem("studentName");
 
+// Timer
+function startTimer() {
+
+    let timer = setInterval(function () {
+
+        let minutes = Math.floor(timeLeft / 60);
+        let seconds = timeLeft % 60;
+
+        document.getElementById("timer").innerHTML =
+            minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+
+        timeLeft--;
+
+        if (timeLeft < 0) {
+
+            clearInterval(timer);
+
+            localStorage.setItem("answers", JSON.stringify(answers));
+
+            alert("Time is Over!");
+
+            window.location.href = "result.html";
+        }
+
+    }, 1000);
+
+}
+
+// Load Questions
 fetch("questions.json")
 .then(response => response.json())
 .then(data => {
+
     questions = data;
+
     showQuestion();
+
+    startTimer();
+
 });
 
-function showQuestion(){
+// Show Question
+function showQuestion() {
 
     let q = questions[currentQuestion];
 
+    document.getElementById("questionNumber").innerHTML =
+        "Question " + (currentQuestion + 1) + " of " + questions.length;
+
     document.getElementById("question").innerHTML =
-    "Q" + (currentQuestion + 1) + ". " + q.question;
+        "Q" + (currentQuestion + 1) + ". " + q.question;
 
     let html = "";
 
-    q.options.forEach((option,index)=>{
+    q.options.forEach((option, index) => {
 
         html += `
         <div class="option">
-            <input type="radio"
-                   name="answer"
-                   value="${index}"
-                   ${answers[currentQuestion]==index ? "checked":""}>
+            <input
+                type="radio"
+                name="answer"
+                value="${index}"
+                ${answers[currentQuestion] == index ? "checked" : ""}>
             ${option}
         </div>
         `;
@@ -37,33 +78,50 @@ function showQuestion(){
 
     document.getElementById("options").innerHTML = html;
 
+    let btn = document.getElementById("nextBtn");
+
+    if (currentQuestion == questions.length - 1) {
+        btn.innerHTML = "Finish Test";
+    } else {
+        btn.innerHTML = "Next";
+    }
+
 }
 
-function nextQuestion(){
+// Next Question
+function nextQuestion() {
 
     let selected =
-    document.querySelector('input[name="answer"]:checked');
+        document.querySelector('input[name="answer"]:checked');
 
-    if(selected){
-
-        answers[currentQuestion] =
-        Number(selected.value);
-
+    if (selected) {
+        answers[currentQuestion] = Number(selected.value);
     }
 
     currentQuestion++;
 
-    if(currentQuestion >= questions.length){
+    if (currentQuestion >= questions.length) {
 
-        localStorage.setItem("answers",
-        JSON.stringify(answers));
+        localStorage.setItem("answers", JSON.stringify(answers));
 
-        window.location.href="result.html";
+        window.location.href = "result.html";
 
         return;
-
     }
 
     showQuestion();
+
+}
+
+// Previous Question
+function previousQuestion() {
+
+    if (currentQuestion > 0) {
+
+        currentQuestion--;
+
+        showQuestion();
+
+    }
 
 }
