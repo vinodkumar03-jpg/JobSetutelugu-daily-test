5d048e0dfb3fd49271de515d3277a444b67227b6
vinodkumar03-jpg <vinodkumarvankudoth011@gmail.com>
2026-08-01 10:35:11 +0530

    Add reset password HTML structure

--- js/reset-password.js
+++ js/reset-password.js
@@ -1,1 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Reset Password | JobSetu Telugu</title>
+
+<link rel="icon" href="images/logo.png">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+<link rel="stylesheet" href="css/reset-password.css">
+
+</head>
+
+<body>
+
+<div class="container">
+
+    <div class="card">
+
+        <img src="images/logo.png" class="logo">
+
+        <h2>Create New Password</h2>
+
+        <p>Choose a strong password for your JobSetu Telugu account.</p>
+
+        <div class="input-box">
+
+            <i class="fa-solid fa-lock"></i>
+
+            <input type="password" id="newPassword" placeholder="New Password">
+
+        </div>
+
+        <div class="input-box">
+
+            <i class="fa-solid fa-lock"></i>
+
+            <input type="password" id="confirmPassword" placeholder="Confirm Password">
+
+        </div>
+
+        <button id="resetBtn">
+
+            <i class="fa-solid fa-key"></i>
+
+            Update Password
+
+        </button>
+
+        <div class="back">
+
+            <a href="index.html">
+
+                <i class="fa-solid fa-arrow-left"></i>
+
+                Back to Login
+
+            </a>
+
+        </div>
+
+    </div>
+
+</div>
+
+<script src="js/reset-password.js"></script>
+
+</body>
+
+</html>
